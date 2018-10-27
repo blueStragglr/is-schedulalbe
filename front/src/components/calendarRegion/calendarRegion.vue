d4eed18357b0a91a5727e44641b8ea34632723bb
blueStragglr <oortcloud1599@artifriends.com>
2018-10-27 17:57:30 +0900

    commit for new accont

---
+++ front/src/components/calendarRegion/calendarRegion.vue
@@ -0,0 +1,28 @@
+<template>
+    <div id="calendarRegion">
+      <month-picker></month-picker>
+      <calendar></calendar>
+    </div>
+</template>
+
+<script>
+import monthPicker from '@/components/calendarRegion/monthPicker'
+import calendar from '@/components/calendarRegion/calendar'
+export default {
+  name: "calendar-region",
+  components: {
+    monthPicker,
+    calendar
+  }
+}
+</script>
+
+<style scoped>
+    @media screen and (min-width: 768px){
+        #calendarRegion{
+            display: inline-block;
+            width: 70%;
+            padding: 50px 10%;
+        }
+    }
+</style>
